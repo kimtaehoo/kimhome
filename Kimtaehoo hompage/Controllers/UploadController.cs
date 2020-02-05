b7f6c39b70c3042ece6dfe383ed98cb364bcbbbb
kimtaehoo <59912332+kimtaehoo@users.noreply.github.com>
2020-02-05 15:27:13 +0900

    init

--- Kimtaehoo hompage/Controllers/UploadController.cs
+++ Kimtaehoo hompage/Controllers/UploadController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Kimtaehoo_hompage.Controllers
 {
@@ -14,19 +16,21 @@ namespace Kimtaehoo_hompage.Controllers
             _environment = environment;
         }
         [HttpPost, Route("api/upload")]
-        public IActionResult ImageUpload(IFormFile file) // 특정 파일을 선택했을 때 input 박스에 담긴 파일을 전송 버튼을 눌렀을때 file에 담김
+        public async Task<IActionResult> ImageUpload(IFormFile file) // 특정 파일을 선택했을 때 input 박스에 담긴 파일을 전송 버튼을 눌렀을때 file에 담김
         {
             // # 이미지나 파일을 업로드 할 때 필요한 구성
             // 1. path(경로) - 어디에다 저장할지 결정
             var path = Path.Combine(_environment.WebRootPath, @"images\upload");
             // 2. name(이름) - DateTime, GUID *GUID 전역 고유 식별자 
-            var fileName = file.FileName;
+            // 파일 이름 image.jpg
+            var fileFullName = file.FileName.Split('.');
+            var fileName = $"{Guid.NewGuid()}.{fileFullName[1]}";
             // 3. Extension(확장자) - jpg, png ... txt
             using (var fileStream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
             {
-                file.CopyTo(fileStream);
-            }
-            return Ok();
+                await file.CopyToAsync(fileStream);
+            } 
+            return Ok(new { file= "/images/upload/" + fileName, success = true});
 
             // # URL 접근 방식
             // ASP.NET - 호스트명/ + api/upload
